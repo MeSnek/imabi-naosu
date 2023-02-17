d1541a027cf40c94da5e4a41d58e56061b071155
sneku <sneakyfox61@gmail.com>
2023-02-16 22:22:03 -0500

    ok fr functions build.

--- imabi-naosu.js
+++ imabi-naosu.js
@@ -18,15 +18,15 @@
     var widthElem = document.querySelector(".wrap-main");
 
     changePosition(positionElem);
-    changeWidth(width_elem)
+    changeWidth(widthElem)
 
     //changes the actual width of the box to let more text sit in it so it doesnt look like a mobile thing
     function changeWidth(elem) {
-        width_elem.style.width = "800px";
+        elem.style.width = "800px";
     }
     //changes the position of the box
     function changePosition(elem) {
-        positionElem.style.margin = "0px";
-        positionElem.style.width = "1425px";
+        elem.style.margin = "0px";
+        elem.style.width = "1425px";
     }
 })();
